8ab79aa439ad4fc5ef67706dc0df25493a163ee0
matheuskk9071-tech <matheuskk9071@gmail.com>
2026-02-26 20:50:27 -0400

    Update and rename package.json to package.js

--- package.js
+++ package.js
@@ -1,10 +1,14 @@
 {
   "name": "Tsuo Payments",
   "version": "1.0.0",
+  "private": true,
   "main": "index.js",
   "type": "commonjs",
   "scripts": {
     "start": "node index.js"
+  },
+  "engines": {
+    "node": ">=18"
   },
   "dependencies": {
     "discord.js": "^14.16.0",
